4b9e67d1f5d061e8e5d1d2bb2ae8980f29b0b51e
me-Asad <162339255+me-Asad@users.noreply.github.com>
2024-09-30 07:38:52 -0700

    Add files via upload

---
+++ asad c#/class2/class2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int val1 = int.Parse(textBox1.Text);
+            int val2 = int.Parse(textBox2.Text);
+            int result = val1 + val2;
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int val1 = int.Parse(textBox1.Text);
+            int val2 = int.Parse(textBox2.Text);
+            int result = val1 - val2;
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int val1 = int.Parse(textBox1.Text);
+            int val2 = int.Parse(textBox2.Text);
+            int result = val1 / val2;
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int val1 = int.Parse(textBox1.Text);
+            int val2 = int.Parse(textBox2.Text);
+            int result = val1 * val2;
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
